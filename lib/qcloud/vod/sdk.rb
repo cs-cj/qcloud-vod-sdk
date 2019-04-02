923d3255d190b0ab912bfe1d8b217c4ef59ef216
jack qin <jackqin@jackdeMacBook-Pro.local>
2019-04-02 13:01:16 +0800

    init gem

---
+++ lib/qcloud/vod/sdk.rb
@@ -0,0 +1,10 @@
+require "qcloud/vod/sdk/version"
+
+module Qcloud
+  module Vod
+    module Sdk
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/qcloud/vod/sdk/version.rb
@@ -0,0 +1,7 @@
+module Qcloud
+  module Vod
+    module Sdk
+      VERSION = "0.1.0"
+    end
+  end
+end
